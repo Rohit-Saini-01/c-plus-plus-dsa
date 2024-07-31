6606fad99b7fdca9a80fbe6924991185f32afb28
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-31 17:34:06 +0530

    arithmetic operators

---
+++ 01-Operators/00-ArithmeticOperators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num_1 = 2;
+    int num_2 = 3;
+
+    cout << num_1 <<" + " << num_2 << " = " << num_1 + num_2 << endl;
+    cout << num_1 <<" - " << num_2 << " = " << num_1 - num_2 << endl;
+    cout << num_1 <<" * " << num_2 << " = " << num_1 * num_2 << endl;
+    cout << num_1 <<" / " << num_2 << " = " << num_1 / num_2 << endl;
+    cout << num_1 <<" % " << num_2 << " = " << num_1 % num_2 << endl;
+
+    return 0;
+}
