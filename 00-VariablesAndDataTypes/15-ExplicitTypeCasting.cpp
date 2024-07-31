b5e4ff3c144ee4f3abdf9e1dda2e0bee1cf31cbb
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-31 17:20:03 +0530

    added some examples

--- 00-VariablesAndDataTypes/15-ExplicitTypeCasting.cpp
+++ 00-VariablesAndDataTypes/15-ExplicitTypeCasting.cpp
@@ -7,7 +7,11 @@ int main()
     cout << (int)PI << "\n";
 
     cout << (float)10 / 3<<endl;
-    cout << (char)('A' + 1);
+    cout << (char)('A' + 1)<<endl;
+
+    cout << ((bool)3 + 2)<<endl; // 3 is explicitly typecasted to 1 that is integral value for true
+
+    cout << (23.5 + 2 + 'A'); // Implicitly type casted to double (90.5)
 
     return 0;
 }
