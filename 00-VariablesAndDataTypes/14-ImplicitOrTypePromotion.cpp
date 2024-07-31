40028812cf795bed1e0551fa57222e8c7b0007c8
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-31 17:10:31 +0530

    added one more example

--- 00-VariablesAndDataTypes/14-ImplicitOrTypePromotion.cpp
+++ 00-VariablesAndDataTypes/14-ImplicitOrTypePromotion.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main()
 {
-    cout << (10 / 3)<<"\n";
-    cout << (10 / 3.0); // 10 is automatically converted to float or double by complier to prevent data loss.
+    cout << (10 / 3) << "\n";
+    cout << (10 / 3.0) << "\n"; // 10 is automatically converted to float or double by complier to prevent data loss.
+
+    cout << 'a' + 1;
 
     return 0;
 }
