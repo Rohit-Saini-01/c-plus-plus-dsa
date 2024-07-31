f14d7b0c7d58542f0d07fe6e35a017de1732b335
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-31 13:30:36 +0530

    comments in c++

---
+++ 00-VariablesAndDataTypes/04-SumOfTwoIntegers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num_1 = 10;
+    int num_2 = 5;
+    int sum = num_1 + num_2;
+
+    cout << "Sum of two integers is: " << sum;
+
+    return 0;
+}
