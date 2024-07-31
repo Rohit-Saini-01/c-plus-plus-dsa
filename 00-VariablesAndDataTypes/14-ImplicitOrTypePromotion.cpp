018c9270b9e2e351fc78256af3cf8c3497f11bf3
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-31 17:07:17 +0530

    constants and implicit type conversion

---
+++ 00-VariablesAndDataTypes/14-ImplicitOrTypePromotion.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << (10 / 3)<<"\n";
+    cout << (10 / 3.0); // 10 is automatically converted to float or double by complier to prevent data loss.
+
+    return 0;
+}
