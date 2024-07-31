cc37a7688cf277bab2d24c15e340bcaf9903ea41
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-31 13:27:05 +0530

    datatypes in c++

---
+++ 00-VariablesAndDataTypes/03-DataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char var_1 = 'a';
+    bool var_2 = true;
+    int var_3 = 10;
+    float var_4 = 3.14f;
+    double var_5 = 3.1415;
+
+    cout << var_1 << "\n"
+         << var_2 << "\n"
+         << var_3 << "\n"
+         << var_4 << "\n"
+         << var_5 << "\n";
+
+    return 0;
+}
