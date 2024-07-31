8973a17f1dde39bb93a0eb3596e77afaae396f11
Rohit Saini <emailrohitsaini@gmail.com>
2024-07-31 17:13:21 +0530

    type casting

---
+++ 00-VariablesAndDataTypes/15-ExplicitTypeCasting.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float PI = 3.14f;
+    cout << (int)PI << "\n";
+
+    cout << (float)10 / 3<<endl;
+    cout << (char)('A' + 1);
+
+    return 0;
+}
